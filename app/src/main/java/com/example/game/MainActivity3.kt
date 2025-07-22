6796c6548a4244ae35fcfa12303b6b36b495e823
Fereden <saiffarit11@gmail.com>
2025-07-22 16:58:49 +0300

    demoDesign

---
+++ app/src/main/java/com/example/game/MainActivity3.kt
@@ -0,0 +1,31 @@
+package com.example.game
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main3)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun menu(view: View) {
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    fun end(view: View) {
+        startActivity(Intent(this, MainActivity8::class.java))
+    }
+
+}
